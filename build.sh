1d989408df3174cfe84072c8f9a7998639414467
maclinbc <maclinbc@evms.edu>
2025-03-12 11:52:04 -0400

    Add ML model download and update .gitignore
    Ensure the required model directory exists and add logic to download the ML model from Google Cloud Storage during the build process. Update .gitignore to exclude the downloaded model file from version control.

--- build.sh
+++ build.sh
@@ -4,5 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Ensure the model directory exists
+mkdir -p DermaLytica/Prediction_Model/AI_Models
+
+# Download the ML model from Google Cloud Storage
+wget -O DermaLytica/Prediction_Model/AI_Models/KERAS_model.tflite "https://storage.googleapis.com/dermalyticsdrive/models/KERAS_model.tflite"
+
 # Convert static asset files
 python manage.py collectstatic --no-input
