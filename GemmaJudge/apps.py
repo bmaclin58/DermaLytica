d456d90b08635b0ef1a665d12a3caf7f0332d218
Brian Maclin <bmacli6@wgu.edu>
2026-04-30 23:49:47 -0400

    Update GemmaJudge app configuration and add PortfolioHome link
    - Renamed `GemmaJudge` app to fix app name inconsistency.
    - Added GemmaJudge link and thumbnail to `PortfolioHome.html` for better navigation.
    - Included new static assets for enhanced visual presentation.

--- GemmaJudge/apps.py
+++ GemmaJudge/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class GemmaJudgeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'Gemma_Judge'
+    name = 'GemmaJudge'
 
     def ready(self):
         from .services.runtime import bootstrap_runtime
